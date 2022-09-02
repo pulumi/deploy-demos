c96daa7df0aef7bd018461deb4b5c8698b34c841
evanboyle <evan@pulumi.com>
2022-09-02 16:46:57 -0700

    update command harness to support easily deploying multiple projects, add bucket-time project

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Kicking the tires on Pulumi Deploy. A collection of demos and a program to drive
 Pre-reqs:
 1. [pulumi access token ](https://www.pulumi.com/docs/intro/pulumi-service/accounts/#creating-access-tokens)
 2. [GitHub PAT](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) (I believe this needs at least repo priveleges)
+3. AWS creds (for some demos)
 
 To get started:
 
@@ -16,6 +17,7 @@ To get started:
 $ yarn install
 $ export PULUMI_ACCESS_TOKEN=...
 $ export GITHUB_ACCESS_TOKEN=...
+$ # splat your temp AWS creds into env vars
 $ yarn start
 ```
 
--- commandAndControl/index.ts
+++ commandAndControl/index.ts
@@ -1,7 +1,8 @@
 const org = "pulumi";
-const project = "simple-resource"
 const stack = "dev"
 const backendURL = "http://api.pulumi.com/api"
+
+type SupportedProject = "simple-resource" | "bucket-time";
 
 const makePulumiAPICall = async (method: string, urlSuffix: string, payload?: any) => {
     const url = `${backendURL}/${urlSuffix}`
@@ -28,8 +29,13 @@ const makePulumiAPICall = async (method: string, urlSuffix: string, payload?: an
     return await response.json();
 }
 
-const createDeployment = async () => {
+const createDeployment = async (project: string, payload: any) => {
     const urlSuffix  = `preview/deployments/${org}/${project}/${stack}`;
+
+    return await makePulumiAPICall('POST', urlSuffix, payload);
+}
+
+const createSimpleDeployment = async () => {
     const payload = {
         sourceContext: {
             git: {
@@ -50,14 +56,53 @@ const createDeployment = async () => {
         }
     };
 
-    return await makePulumiAPICall('POST', urlSuffix, payload);
+    return createDeployment("simple-resource", payload);
 }
 
-const getDeploymentStatus = async (deploymentID: string) => {
+const createAwsBucketDeployment = async () => {
+    const payload = {
+        sourceContext: {
+            git: {
+                repoURL: "https://github.com/pulumi/deploy-demos.git",
+                branch: "refs/heads/main",
+                repoDir: "bucket-time",
+                gitAuth: {
+                    accessToken: process.env.GITHUB_ACCESS_TOKEN,
+                }
+            }
+        },
+        operationContext: {
+            operation: "update",
+            preRunCommands: [
+            ],
+            environmentVariables: {
+                AWS_REGION: "us-west-2",
+                AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+                AWS_SESSION_TOKEN: process.env.AWS_SESSION_TOKEN,
+            }
+        }
+    };
+
+    return createDeployment("bucket-time", payload);
+}
+
+const createProjectDeployment = async (project: SupportedProject) => {
+    switch(project) {
+        case "simple-resource":
+            return createSimpleDeployment();
+        case "bucket-time":
+            return createAwsBucketDeployment();
+        default:
+            throw new Error(`unable to deploy project. unknown project: ${project}`);
+    }
+}
+
+const getDeploymentStatus = async (project: string, deploymentID: string) => {
    return await makePulumiAPICall("GET", `preview/deployments/${org}/${project}/${stack}/${deploymentID}`)
 }
 
-const getDeploymentLogs = async (deploymentID: string) => {
+const getDeploymentLogs = async (project: string, deploymentID: string) => {
     return await makePulumiAPICall("GET", `preview/deployments/${org}/${project}/${stack}/${deploymentID}/logs`)
  }
 
@@ -65,30 +110,32 @@ function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-const printStatusAndLogs = async (deploymentID: string) =>{
-    const deploymentStatusResult = await getDeploymentStatus(deploymentID);
+const printStatusAndLogs = async (project: string, deploymentID: string) =>{
+    const deploymentStatusResult = await getDeploymentStatus(project, deploymentID);
     console.log(deploymentStatusResult);
-    const deploymentLogs = await getDeploymentLogs(deploymentID);
+    const deploymentLogs = await getDeploymentLogs(project, deploymentID);
     console.log(JSON.stringify(deploymentLogs));
 }
 
 const nonTerminalDeploymentStatuses = ["not-started", "accepted", "running"];
 
 const run = async () => {
-    const deploymentResult = await createDeployment();
+    const project: SupportedProject = "bucket-time";
+    const deploymentResult = await createProjectDeployment(project)
     console.log(deploymentResult);
     let status = "not-started"
     while (nonTerminalDeploymentStatuses.includes(status) || !status) {
-        const deploymentStatusResult = await getDeploymentStatus(deploymentResult.id);
+        const deploymentStatusResult = await getDeploymentStatus(project, deploymentResult.id);
         status = deploymentStatusResult.status; 
         console.log(deploymentStatusResult);
 
-        const deploymentLogs = await getDeploymentLogs(deploymentResult.id);
+        const deploymentLogs = await getDeploymentLogs(project, deploymentResult.id);
         console.log(JSON.stringify(deploymentLogs));
 
         await delay(2000);
     }
 
+    // useful for debugging the driver if it happens to exit early before a deployment has finished (ie an API field changes)
     // printStatusAndLogs("79fcd545-f9f0-4287-8c53-06072f508732")
 
 }
