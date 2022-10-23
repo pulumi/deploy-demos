cf8cdfc3d0b9158cc5f460a025d327e888db0a22
evanboyle <evan@pulumi.com>
2022-10-22 20:14:25 -0700

    change the name of the bucket

--- pulumi-programs/aws-ts-s3/index.ts
+++ pulumi-programs/aws-ts-s3/index.ts
@@ -3,7 +3,7 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("my-bucket-foo");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
