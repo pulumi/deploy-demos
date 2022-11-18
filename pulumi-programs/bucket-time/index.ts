3d666e792cdbb2e2a720344c40ff4d2a5da1ecc8
Pat Gavlin <pat@pulumi.com>
2022-11-17 21:07:00 -0800

    gcp

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -1,9 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as gcp from "@pulumi/gcp";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const s3Bucket = new aws.s3.Bucket("my-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Create a GCP resource (GCS Bucket)
+const gcsBucket = new gcp.storage.Bucket("my-bucket", {
+    forceDestroy: true,
+    location: "US",
+    publicAccessPrevention: "enforced",
+    uniformBucketLevelAccess: true,
+});
+
+// Export the names of the buckets
+export const s3BucketName = s3Bucket.id;
+export const gcsBucketUrl = gcsBucket.url;
