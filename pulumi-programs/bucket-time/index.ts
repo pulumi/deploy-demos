983975392dee06315a55b31d3119b52008e9f430
Pat Gavlin <pat@pulumi.com>
2023-04-17 15:35:15 -0700

    reenable gcp

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -20,14 +20,14 @@ const storageContainer = new azure.storage.Container("my-storage-container", {
 });
 
 // Create a GCP resource (GCS Bucket)
-//const gcsBucket = new gcp.storage.Bucket("my-bucket", {
-//    forceDestroy: true,
-//    location: "US",
-//    publicAccessPrevention: "enforced",
-//    uniformBucketLevelAccess: true,
-//});
+const gcsBucket = new gcp.storage.Bucket("my-bucket", {
+    forceDestroy: true,
+    location: "US",
+    publicAccessPrevention: "enforced",
+    uniformBucketLevelAccess: true,
+});
 
 // Export the names of the buckets
 export const s3BucketName = s3Bucket.id;
 export const storageContainerName = storageContainer.id;
-//export const gcsBucketUrl = gcsBucket.url;
+export const gcsBucketUrl = gcsBucket.url;
