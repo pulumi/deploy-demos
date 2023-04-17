196b79b12c2ccc75d081f25e1f60007797f92c84
Pat Gavlin <pat@pulumi.com>
2023-04-17 14:27:40 -0700

    skip gcp for now

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as azure from "@pulumi/azure";
-import * as gcp from "@pulumi/gcp";
+//import * as gcp from "@pulumi/gcp";
 
 // Create an AWS resource (S3 Bucket)
 const s3Bucket = new aws.s3.Bucket("my-bucket");
@@ -20,14 +20,14 @@ const storageContainer = new azure.storage.Container("my-storage-container", {
 });
 
 // Create a GCP resource (GCS Bucket)
-const gcsBucket = new gcp.storage.Bucket("my-bucket", {
-    forceDestroy: true,
-    location: "US",
-    publicAccessPrevention: "enforced",
-    uniformBucketLevelAccess: true,
-});
+//const gcsBucket = new gcp.storage.Bucket("my-bucket", {
+//    forceDestroy: true,
+//    location: "US",
+//    publicAccessPrevention: "enforced",
+//    uniformBucketLevelAccess: true,
+//});
 
 // Export the names of the buckets
 export const s3BucketName = s3Bucket.id;
 export const storageContainerName = storageContainer.id;
-export const gcsBucketUrl = gcsBucket.url;
+//export const gcsBucketUrl = gcsBucket.url;
