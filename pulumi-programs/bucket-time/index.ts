efc19e5d32b36a44d1012178a8d6cc2435d0fab4
Pat Gavlin <pat@pulumi.com>
2023-05-26 14:09:24 -0700

    [bucket-time] Add a bucket object.

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -5,5 +5,12 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket).
 const bucket = new aws.s3.Bucket("my-bucket");
 
+// Create a bucket object.
+new aws.s3.BucketObject("my-bucket-object", {
+    bucket: bucket,
+    key: "my-bucket-object",
+    content: "bucket object!",
+});
+
 // Export the name of the bucket.
 export const bucketName = bucket.id;
