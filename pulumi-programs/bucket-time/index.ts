dec30ab3da42367795eddaf25b028a0c5804554b
Pat Gavlin <pat@pulumi.com>
2023-11-29 10:41:52 -0800

    update to static site example

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -1,16 +1,56 @@
-import * as pulumi from "@pulumi/pulumi";
+// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
+import * as fs from "fs";
+import * as mime from "mime";
 
-// Create an AWS resource (S3 Bucket).
-const bucket = new aws.s3.Bucket("my-bucket");
-
-// Create a bucket object.
-new aws.s3.BucketObject("my-bucket-object", {
-    bucket: bucket,
-    key: "my-bucket-object",
-    content: "bucket object!",
+// Create a bucket and expose a website index document
+const siteBucket = new aws.s3.Bucket("s3-website-bucket", {
+    website: {
+        indexDocument: "index.html",
+    },
 });
 
-// Export the name of the bucket.
-export const bucketName = bucket.id;
+// Enable public access to the bucket.
+const publicBlock = new aws.s3.BucketPublicAccessBlock("s3-website-bucket", {
+    bucket: siteBucket.id,
+    blockPublicAcls: false,
+    blockPublicPolicy: false,
+    ignorePublicAcls: false,
+    restrictPublicBuckets: false,
+});
+
+const siteDir = "www"; // directory for content files
+
+// For each file in the directory, create an S3 object stored in `siteBucket`
+for (const item of fs.readdirSync(siteDir)) {
+    const filePath = require("path").join(siteDir, item);
+    const siteObject = new aws.s3.BucketObject(item, {
+        bucket: siteBucket,                               // reference the s3.Bucket object
+        source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
+        contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
+    });
+}
+
+// Set the access policy for the bucket so all objects are readable
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.id, // refer to the bucket created earlier
+    policy: pulumi.jsonStringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject",
+            ],
+            Resource: [
+                pulumi.interpolate `${siteBucket.arn}/*`,
+            ],
+        }],
+    }),
+}, {dependsOn: publicBlock});
+
+// Stack exports.
+export const bucketName = siteBucket.bucket;
+export const websiteUrl = pulumi.interpolate`http://${siteBucket.websiteEndpoint}`;
